f053f333a5efa7230ed4d98a2498559623e78ba2
moimenwagdy <moimenwy@gmail.com>
2024-11-20 17:27:22 +0200

    created the new post form , with emojis and feelings selections options, styled and prepared to post to DB

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,7 +2,7 @@ import { getTranslations } from "next-intl/server";
 import HomePageBackGround from "../components/Home/BackGround/HomePageBackGround";
 import Description from "../components/Home/Description/Description";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
-import Image from "next/image";
+import MainHeaderAdress from "../components/Home/MainHeaderAdress/MainHeaderAdress";
 
 export default async function Home({ params }: Params) {
   const tHome = await getTranslations("homePage");
@@ -12,12 +12,7 @@ export default async function Home({ params }: Params) {
     <main className="relative md:h-screen overflow-hidden z-0 ">
       <HomePageBackGround />
       <div className=" text-center">
-        <h1
-          className={`text-6xl md:text-8xl text-headed dark:text-offWhite   ${
-            ar ? "font-arBasic font-extrabold" : "font-heavyFont"
-          }   mt-[30%] md:mt-[15%] -translate-y-[50%]`}>
-          {tHome("Name")}
-        </h1>
+        <MainHeaderAdress />
         <Description />
       </div>
     </main>
---
+++ app/components/Home/MainHeaderAdress/MainHeaderAdress.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { useLocale, useTranslations } from "next-intl";
+
+const MainHeaderAdress = () => {
+  const tHome = useTranslations("homePage");
+  const locale = useLocale();
+  const ar = locale === "ar";
+
+  return (
+    <h1
+      className={`text-6xl md:text-8xl text-headed dark:text-offWhite   ${
+        ar ? "font-arBasic font-extrabold" : "font-heavyFont"
+      }   mt-[30%] md:mt-[15%] -translate-y-[50%]`}>
+      {tHome("Name")}
+    </h1>
+  );
+};
+
+export default MainHeaderAdress;
