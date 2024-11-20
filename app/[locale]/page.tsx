6eab6cbdd407041db61bdacff3f934a8bb7bdaa7
moimenwagdy <moimenwy@gmail.com>
2024-11-20 17:31:09 +0200

    created the new post form , with emojis and feelings selections options, styled and prepared to post to DB

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,13 +1,8 @@
-import { getTranslations } from "next-intl/server";
 import HomePageBackGround from "../components/Home/BackGround/HomePageBackGround";
 import Description from "../components/Home/Description/Description";
-import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import MainHeaderAdress from "../components/Home/MainHeaderAdress/MainHeaderAdress";
 
-export default async function Home({ params }: Params) {
-  const tHome = await getTranslations("homePage");
-  const locale = params.locale;
-  const ar = locale === "ar";
+export default async function Home() {
   return (
     <main className="relative md:h-screen overflow-hidden z-0 ">
       <HomePageBackGround />
