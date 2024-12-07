089eeace3aa224df27b70c7e4f38f78a8f68c809
moimenwagdy <moimenwy@gmail.com>
2024-12-08 01:06:02 +0200

    fixed type error

--- app/[locale]/user/[userID]/page.tsx
+++ app/[locale]/user/[userID]/page.tsx
@@ -2,8 +2,8 @@ import UserProfile from "@/app/components/UserProfile/UserProfile";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import React from "react";
 
-const page = ({ params }: Params) => {
+const Page = ({ params }: { params: Params }) => {
   return <UserProfile userID={params.userID} />;
 };
 
-export default page;
+export default Page;
