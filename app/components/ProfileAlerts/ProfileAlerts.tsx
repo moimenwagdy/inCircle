54c1c9727fbd81f6aa56c5f0c8be3567a870cd49
moimenwagdy <moimenwy@gmail.com>
2024-12-20 22:04:35 +0200

    notification now works for likes comments asn following users, notifications buttons style updated, browser notification works with the user permision

--- app/components/ProfileAlerts/ProfileAlerts.tsx
+++ app/components/ProfileAlerts/ProfileAlerts.tsx
@@ -10,7 +10,7 @@ const ProfileAlerts = () => {
   return (
     <>
       {session?.data && (
-        <div className="flex justify-center items-center gap-x-2 h-fit px-20 py-1 fixed top-20 left-1/2 -translate-x-[50%]">
+        <div className="flex justify-center items-center gap-x-2 h-fit px-20 py-1 fixed top-20 left-1/2 -translate-x-[50%] z-50">
           <NotificationsContainer userID={session?.data.user._id!} />
           <MessagesButton />
         </div>
