be5b315c7b453def2ed5f36008bfa73ecee71a3f
moimenwagdy <moimenwy@gmail.com>
2024-12-20 21:53:51 +0200

    notification now works for likes comments asn following users, notifications buttons style updated, browser notification works with the user permision

--- lib/sendBrowserNotification.ts
+++ lib/sendBrowserNotification.ts
@@ -1,20 +1,24 @@
 export const sendBrowserNotification = (title: string, message: string) => {
-  if (Notification.permission === "denied") {
-    return;
-  }
-  if (Notification.permission === "granted") {
-    new Notification(title, {
-      body: message,
-    });
-  } else if (Notification.permission === "default") {
-    Notification.requestPermission().then((permission) => {
-      if (permission === "granted") {
-        new Notification(title, {
-          body: message,
-        });
-      } else {
-        return;
-      }
-    });
+  if ("Notification" in window) {
+    if (Notification.permission === "denied") {
+      return;
+    }
+    if (Notification.permission === "granted") {
+      new Notification(title, {
+        body: message,
+      });
+    } else if (Notification.permission === "default") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          new Notification(title, {
+            body: message,
+          });
+        } else {
+          return;
+        }
+      });
+    }
+  } else {
+    console.warn("Browser does not support notifications.");
   }
 };
