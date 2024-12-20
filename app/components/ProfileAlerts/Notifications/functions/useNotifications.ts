79ecc52c0cd477381e6b98e6ee992e89b8edcb16
moimenwagdy <moimenwy@gmail.com>
2024-12-20 21:56:35 +0200

    notification now works for likes comments asn following users, notifications buttons style updated, browser notification works with the user permision

--- app/components/ProfileAlerts/Notifications/functions/useNotifications.ts
+++ app/components/ProfileAlerts/Notifications/functions/useNotifications.ts
@@ -1,28 +1,31 @@
 import { useQuery } from "@tanstack/react-query";
-import { useState, useEffect } from "react";
-import { sendBrowserNotification } from "@/lib/sendBrowserNotification";
+// import { useState, useEffect } from "react";
+// import { sendBrowserNotification } from "@/lib/sendBrowserNotification";
 import { getUserNotifications } from "./getUserNotifications";
 
 export const useNotifications = (userID: string) => {
-  const { data, isSuccess } = useQuery<[]>({
+  const {
+    data,
+    //  isSuccess
+  } = useQuery<[]>({
     queryKey: ["notifications", userID],
     queryFn: async () => await getUserNotifications(userID),
     refetchInterval: 20000,
     refetchIntervalInBackground: true,
   });
 
-  const [prevCount, setPrevCount] = useState<number>(0);
+  //   const [prevCount, setPrevCount] = useState<number>(0);
 
-  useEffect(() => {
-    if (isSuccess && data.length > prevCount) {
-      const newNotifications = data.length - prevCount;
-      sendBrowserNotification(
-        "inCircle Notification",
-        `You have ${newNotifications} new notifications`
-      );
-      setPrevCount(data.length);
-    }
-  }, [data, isSuccess]);
+  //   useEffect(() => {
+  //     if (isSuccess && data.length > prevCount) {
+  //       const newNotifications = data.length - prevCount;
+  //       sendBrowserNotification(
+  //         "inCircle Notification",
+  //         `You have ${newNotifications} new notifications`
+  //       );
+  //       setPrevCount(data.length);
+  //     }
+  //   }, [data, isSuccess]);
 
   return {
     notifications: data ?? [],
