a752d4acb7a15cb9e4a6dc65d955ede4b340e931
moimenwagdy <moimenwy@gmail.com>
2025-01-24 00:44:42 +0200

    SEO test

--- app/[locale]/user/[userID]/layout.tsx
+++ app/[locale]/user/[userID]/layout.tsx
@@ -3,6 +3,7 @@ import getUserProfileData from "@/app/components/UserProfile/functions/getUserPr
 import ProfileNavbar from "@/app/components/UserProfile/ProfileNavbar/ProfileNavbar";
 import UserProfile from "@/app/components/UserProfile/UserProfile";
 import { user } from "@/globalTypes/globalTypes";
+import { getLocale } from "next-intl/server";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import React, { Suspense } from "react";
 
@@ -13,7 +14,45 @@ export const generateMetadata = async ({
 }) => {
   const { userID } = params;
   const response: user = await getUserProfileData(userID);
-  return { title: response.username };
+  const locale = await getLocale();
+  const { username, gender, status, email, age, profile } = response;
+
+  return {
+    title: `Profile of ${username} | inCircle`,
+    description: `Discover ${username}'s profile on inCircle. Connect, chat, and share moments. Age: ${age}, Email: ${email.slice(
+      0,
+      4
+    )}****${email.slice(8)}, Marital Status: ${status}, Gender: ${gender}.`,
+    keywords: `inCircle, ${username}, social media, user profile, connect, chat, ${gender}, ${status}, new friends, posts, folloing, followers `,
+    openGraph: {
+      type: "profile",
+      title: `${username} | inCircle`,
+      description: `Connect with ${username} on inCircle. Age: ${age}, Email: ${email.slice(
+        0,
+        4
+      )}****${email.slice(8)}, Marital Status: ${status}, Gender: ${gender}.`,
+      images: [
+        {
+          url: profile.avatar || "/default-profile.png",
+          alt: `Profile picture of ${username}`,
+        },
+      ],
+      url: `https://in-circle-iota.vercel.app/${locale}/user/${userID}`,
+      site_name: "inCircle",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `${username} | inCircle`,
+      description: `Meet ${username} on inCircle. Age: ${age}, Email: ${email.slice(
+        0,
+        4
+      )}****${email.slice(8)}, Marital Status: ${status}, Gender: ${gender}.`,
+      images: [profile.avatar || "/default-profile.png"],
+    },
+    alternates: {
+      canonical: `https://in-circle-iota.vercel.app/${locale}/user/${userID}`,
+    },
+  };
 };
 
 const layout = ({
