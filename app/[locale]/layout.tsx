ecc8663e75a934f397c92e70450795696d877c15
moimenwagdy <moimenwy@gmail.com>
2025-01-24 21:49:46 +0200

    updating meta tags

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -15,9 +15,46 @@ import SmallScreenList from "../components/SmallScreenList/SmallScreenList";
 import { afacad } from "@/lib/fonts/basicFont";
 
 export const metadata: Metadata = {
-  title: "inCircle",
+  title: "inCircle – Connect & Share with Loved Ones",
   description:
-    "an online club where family and friends connect, chat, and share moments together.",
+    "inCircle is a private online club where family and friends connect, chat, and share their special moments securely.",
+  keywords: [
+    "inCircle",
+    "social network",
+    "family club",
+    "friends chat",
+    "private community",
+    "secure messaging",
+    "photo sharing",
+    "group communication",
+  ],
+  authors: [{ name: "Moimen Wagdy", url: "https://moo-portfolio.vercel.app/" }],
+  openGraph: {
+    title: "inCircle – Stay Connected with Your Close Ones",
+    description:
+      "Join inCircle, a private space where you can chat, share, and create memories with family and friends.",
+    url: "https://in-circle-iota.vercel.app/",
+    siteName: "inCircle",
+    images: [
+      {
+        url: "https://firebasestorage.googleapis.com/v0/b/incircle-f2a58.appspot.com/o/finalLogoLarg.png?alt=media&token=5bdeac30-cffb-49dd-b563-efe419592ff7",
+        width: 800,
+        height: 800,
+        alt: "inCircle - A Private Online Club",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "inCircle – Connect & Share with Loved Ones",
+    description:
+      "A private space for family and friends to stay connected, chat, and share special moments.",
+    images: [
+      "https://firebasestorage.googleapis.com/v0/b/incircle-f2a58.appspot.com/o/finalLogoLarg.png?alt=media&token=5bdeac30-cffb-49dd-b563-efe419592ff7",
+    ],
+  },
+  metadataBase: new URL("https://in-circle-iota.vercel.app/"),
 };
 
 export default async function RootLayout({
