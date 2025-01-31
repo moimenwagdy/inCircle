3af61e48a9b3046d0237a44588d8d9a457f60baf
moimenwagdy <moimenwy@gmail.com>
2025-02-01 00:15:39 +0200

    SEO improvements

--- app/components/ProfileAlerts/Notifications/TEST.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import StartNewConversation from "../../Messaging/StartNewConversation/StartNewConversation";
-
-const TEST: React.FC<{ ids: string[] }> = ({ ids }) => {
-  return <StartNewConversation participantsIDs={ids} />;
-};
-
-export default TEST;
