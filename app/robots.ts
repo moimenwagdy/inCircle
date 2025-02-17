6ec850014e6a06f6b471277a95e1a3328d480ae4
moimenwagdy <moimenwy@gmail.com>
2025-02-17 18:39:24 +0200

    SEO work

--- app/robots.ts
+++ app/robots.ts
@@ -3,6 +3,6 @@ import { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: { userAgent: "*", allow: "/" },
-    sitemap: "https://in-circle-iota.vercel.app/en/sitemap.xml",
+    sitemap: "https://in-circle-iota.vercel.app/sitemap.xml",
   };
 }
